5e1edf3476411114a7357473bab9c06c8fb4c618
Wang Ziyi <BSGOU@bs-gou.com>
2025-06-11 17:23:55 +0900

    Update app.R

--- app.R
+++ app.R
@@ -1,13 +1,10 @@
 # Launch the ShinyApp (Do not remove this comment)
 # To deploy, run: rsconnect::deployApp()
 # Or use the blue button on top of this file
-if(!require("idepGolem")){
-  remotes::install_github("wong-ziyi/idepGolem")
-}
-
 if (!require("idepGolem", quietly = TRUE)){
   devtools::install_github("Bees-GO/idepGolem", upgrade = "never", force = TRUE)
-  install.packages(".", repos = NULL, type = "source", upgrade = "never", force = TRUE)
+  # If only re-install idepGolem from local, you can run the command below:
+  # install.packages(".", repos = NULL, type = "source", upgrade = "never", force = TRUE)
 }
 
 #pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
